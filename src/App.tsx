cfbbc341ff12c12a6f5da8f3fc351a460077cd92
Petter <petter.fogel@medieinstitutet.se>
2021-08-19 20:44:39 +0200

    initial setup created with react

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './components/UI/navigation';
+
+function App() {
+  return (
+    <div>
+      <Navigation />
+    </div>
+  );
+}
+
+export default App;
