22f5a8f7793b2c9402868606a37ca45ad2cbd61e
Petter <petter.fogel@medieinstitutet.se>
2021-08-20 14:13:23 +0200

    Add burger-menu for responsive ux.

--- src/components/UI/navigation.tsx
+++ src/components/UI/navigation.tsx
@@ -1,20 +1,47 @@
+import { Link } from "react-router-dom";
 import classes from "../../styles/navigation.module.css";
 
 function Navigation() {
-    return (
-        <header className={classes.header}>
-            <div>
-                <h2 className={classes.logo}>Noah</h2>
-            </div>
-            <nav className={classes.nav}>
-                <ul>
-                    <li>Contribute</li>
-                    <li>Products</li>
-                    <li>Contact</li>
-                </ul>
-            </nav>
-        </header>
-    )
+  return (
+    <header className={classes.header}>
+      <div>
+        <h2 className={classes.logo}>Noah</h2>
+      </div>
+      <nav className={classes.nav}>
+        <ul>
+          <li>
+            <Link
+              to="/contribute"
+              style={{ textDecoration: "none", color: "#fff" }}
+            >
+              Contribute
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/products"
+              style={{ textDecoration: "none", color: "#fff" }}
+            >
+              Products
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/contact"
+              style={{ textDecoration: "none", color: "#fff" }}
+            >
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div className={classes.burgerMenu}>
+        <div className={classes.burgerBar}></div>
+        <div className={classes.burgerBar}></div>
+        <div className={classes.burgerBar}></div>
+      </div>
+    </header>
+  );
 }
 
-export default Navigation
+export default Navigation;
